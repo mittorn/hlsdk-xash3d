bfba300fa25e518a176e5d4a5d254e4b62b50f0d
RAINBOW2000 <lorbbbbb@gmail.com>
2017-03-09 00:16:52 +0300

    ...

--- dlls/gearbox/grapple_tonguetip.cpp
+++ dlls/gearbox/grapple_tonguetip.cpp
@@ -112,7 +112,7 @@ void CGrappleTonguetip::TipTouch(CBaseEntity *pOther)
 	m_pMyGrappler->m_fTipHit	= TRUE;
 	m_pMyGrappler->m_iHitFlags	= hitFlags;
 
-	if (hitFlags & (FL_MONSTER))
+	if ((FL_MONSTER|FL_CLIENT))
 	{
 		// Set player attached flag.
 		if (pOther->IsPlayer())
